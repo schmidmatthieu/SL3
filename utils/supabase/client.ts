181bcec980096a35490ba6666e651f1db54531ee
Matthieu Schmid <matthieu@arkstudio.ch>
2024-12-31 11:23:49 +0100

    refactor: remove Supabase integration

--- utils/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-'use client'
-
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/types/supabase'  // Optionnel, pour le typage
-
-export const supabase = createClientComponentClient<Database>()
